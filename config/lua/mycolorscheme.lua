4cd4a81043ee8301383912377e50b046fe863af7
kevinlmadison <coolklm121@gmail.com>
2025-10-04 21:40:08 -0400

    colorscheme

--- config/lua/mycolorscheme.lua
+++ config/lua/mycolorscheme.lua
@@ -10,15 +10,8 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
-vim.cmd("colorscheme habamax")
+vim.cmd("colorscheme gruber-darker")
 
--- vim.cmd [[
---   hi Normal guibg=NONE ctermbg=NONE
---   hi NormalNC guibg=NONE ctermbg=NONE
---   hi SignColumn guibg=NONE ctermbg=NONE
---   hi LineNr guibg=NONE ctermbg=NONE
---   hi EndOfBuffer guibg=NONE ctermbg=NONE
--- ]]
 
 -- Gruvbox
 --
